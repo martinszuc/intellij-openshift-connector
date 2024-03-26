b656811c5bdc4e5e2d6ea24ce93198685eaa50bc
Martin Szuc <mszuc@redhat.com>
2024-03-27 00:10:45 +0100

    added(UItest): public tests GettingStarted, OpenshiftExtension, AboutPublic, LoginDialog
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/test/ui/utils/constants/XPathConstants.java
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/utils/constants/XPathConstants.java
@@ -17,6 +17,14 @@ public class XPathConstants {
     public static final String JPASSWORD_FIELD = "//div[@class='JPasswordField']";
     public static final String CLUSTER_LOGIN_DIALOG = "//div[@accessiblename='Cluster login' and @class='MyDialog']";
     public static final String TREE_CLASS = "//div[@class='Tree']";
+    public static final String MYDIALOG_CLASS = "//div[@class='MyDialog']";
+    public static final String NEXT_BROWSE_ALL_THE_DEVFILE_STACKS = "//div[@accessiblename='Next: Browse all the Devfile stacks from Devfile registries >' and @class='JLabel' and @text='Next: Browse all the Devfile stacks from Devfile registries >']";
+    public static final String BACK_START_A_COMPONENT_IN_DEVELOPMENT_MODE = "//div[@accessiblename='< Back: Start a component in development mode' and @class='JLabel' and @text='< Back: Start a component in development mode']";
+    public static final String LEAVE_FEEDBACK = "//div[@text='Leave feedback']";
+    public static final String SELECT_ALL = "//div[contains(@text.key, 'action.$SelectAll.text')]";
+    public static final String COPY = "//div[contains(@text.key, 'action.$Copy.text')]";
+    public static final String JB_TERMINAL_PANEL = "//div[@class='JBTerminalPanel']";
+    public static final String HIDE_BUTTON = "//div[@class='ToolWindowHeader'][.//div[@class='ContentTabLabel']]//div[@myaction.key='tool.window.hide.action.name']";
 
     public static String getToolWindowButton(String label) {
         return "//div[@tooltiptext='" + label + "']";
