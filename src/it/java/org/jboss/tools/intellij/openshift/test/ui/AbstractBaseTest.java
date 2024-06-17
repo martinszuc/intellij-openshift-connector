a846789f592f98d9e4aec22722d26830712af034
Martin Szuc <mszuc@redhat.com>
2024-06-17 13:15:49 +0200

    fix(UItest): fixed tests for GHA run
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    fix(UItest): debug screenshots
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    fixup! fix(UItest): increased timeout for logout
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    fix(UItest): replaced unnecessary printStackTrace with logger
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    fix(UItest): fix for expand tree issue
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    fix(UItest): increased timeout for logout
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    fix(UItest): remove Kube/config in Suite before tests
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    feat(UItest): added Screenshot
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>
    
    fix(UItest): test order and logout
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/test/ui/AbstractBaseTest.java
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/AbstractBaseTest.java
@@ -16,35 +16,43 @@ import com.redhat.devtools.intellij.commonuitest.fixtures.dialogs.FlatWelcomeFra
 import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.idestatusbar.IdeStatusBar;
 import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.ToolWindowPane;
 import com.redhat.devtools.intellij.commonuitest.utils.project.CreateCloseUtils;
+import com.redhat.devtools.intellij.commonuitest.utils.screenshot.ScreenshotUtils;
 import org.jboss.tools.intellij.openshift.test.ui.annotations.UITest;
 import org.jboss.tools.intellij.openshift.test.ui.dialogs.ProjectStructureDialog;
 import org.jboss.tools.intellij.openshift.test.ui.junit.TestRunnerExtension;
 import org.jboss.tools.intellij.openshift.test.ui.runner.IdeaRunner;
 import org.jboss.tools.intellij.openshift.test.ui.utils.CleanUpUtility;
-import org.jboss.tools.intellij.openshift.test.ui.utils.ProjectUtility;
 import org.jboss.tools.intellij.openshift.test.ui.utils.KubeConfigUtility;
+import org.jboss.tools.intellij.openshift.test.ui.utils.ProjectUtility;
 import org.jboss.tools.intellij.openshift.test.ui.utils.constants.LabelConstants;
-import org.jboss.tools.intellij.openshift.test.ui.utils.constants.XPathConstants;
-import org.jboss.tools.intellij.openshift.test.ui.views.GettingStartedView;
 import org.jboss.tools.intellij.openshift.test.ui.views.OpenshiftView;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.time.Duration;
+
 
 /**
  * @author Ondrej Dockal, odockal@redhat.com
  */
-@ExtendWith(TestRunnerExtension.class)
+@ExtendWith({TestRunnerExtension.class})
 @UITest
 public abstract class AbstractBaseTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractBaseTest.class);
+
     protected static RemoteRobot robot;
     private static boolean isProjectCreatedAndOpened = false;
     public static final String DEFAULT_CLUSTER_URL = "no (current) context/cluster set";
     protected static String currentClusterUrl = DEFAULT_CLUSTER_URL;
+
+    @RegisterExtension
+    TestWatcher testWatcher = new TestWatcherImpl();
 
     @BeforeAll
     public static void setUpProject() {
@@ -67,25 +75,39 @@ public abstract class AbstractBaseTest {
         }
     }
 
-    @AfterEach
-    public void afterEachCleanUp() {
-        CleanUpUtility.cleanUpAll(robot);
-    }
-
+    /**
+     * Logs out from cluster by removing the .kube/config file and refreshing the OpenShift tree view.
+     */
     protected static void logOut() {
-        KubeConfigUtility.removeKubeConfig();
-        sleep(2000);
-        currentClusterUrl = DEFAULT_CLUSTER_URL;
+        LOGGER.info("Starting logout process...");
+        try {
+            KubeConfigUtility.removeKubeConfig();
+            sleep(2000);
+            currentClusterUrl = DEFAULT_CLUSTER_URL;
 
-        OpenshiftView view = robot.find(OpenshiftView.class);
-        view.openView();
-        view.waitForTreeItem(LabelConstants.DEVFILE_REGISTRIES,120,5); // Wait for "loading..." to finish
+            OpenshiftView view = robot.find(OpenshiftView.class);
+            view.openView();
 
-        view.refreshTree(robot);
-        IdeStatusBar ideStatusBar = robot.find(IdeStatusBar.class);
-        ideStatusBar.waitUntilAllBgTasksFinish();
+            IdeStatusBar ideStatusBar = robot.find(IdeStatusBar.class);
+            ideStatusBar.waitUntilAllBgTasksFinish(900);
 
-        view.closeView();
+            view.waitForTreeItem(LabelConstants.DEVFILE_REGISTRIES, 120, 5); // Wait for "loading..." to finish
+
+            view.refreshTree(robot);
+            ideStatusBar.waitUntilAllBgTasksFinish();
+
+            view.closeView();
+            LOGGER.info("Logout process completed successfully.");
+        } catch (Exception e) {
+            IdeStatusBar ideStatusBar = robot.find(IdeStatusBar.class);
+            ideStatusBar.waitUntilAllBgTasksFinish(900);
+
+            OpenshiftView view = robot.find(OpenshiftView.class);
+            view.refreshTree(robot);
+            ideStatusBar.waitUntilAllBgTasksFinish();
+
+            view.closeView();
+        }
     }
 
     public RemoteRobot getRobotReference() {
@@ -103,11 +125,37 @@ public abstract class AbstractBaseTest {
     }
 
     protected static void sleep(long ms) {
-        System.out.println("Putting thread into sleep for: " + ms + " ms");
+        LOGGER.info("Putting thread into sleep for: {} ms", ms);
         try {
             Thread.sleep(ms);
         } catch (InterruptedException e) {
+            LOGGER.error("Sleep interrupted: {}", e.getMessage());
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Captures a screenshot and saves it with the given comment.
+     *
+     * @param comment The comment to add to the screenshot's filename.
+     */
+    protected static void captureScreenshot(String comment) {
+        try {
+            File screenshotFile = ScreenshotUtils.takeScreenshot(robot, comment);
+            if (screenshotFile != null) {
+                LOGGER.info("Screenshot saved at: {}", screenshotFile.getAbsolutePath());
+            } else {
+                LOGGER.warn("Screenshot capture failed.");
+            }
+        } catch (Exception e) {
+            LOGGER.error("Failed to capture screenshot: {}", e.getMessage(), e);
+        }
+    }
+    private static class TestWatcherImpl implements TestWatcher {
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            captureScreenshot("test_failed_" + context.getDisplayName());
+            CleanUpUtility.cleanUpAll(robot);
         }
     }
 }
