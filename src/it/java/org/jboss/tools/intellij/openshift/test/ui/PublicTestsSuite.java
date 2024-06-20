c899f82a9752608890ddcc211c85cb00f8f66d0b
Martin Szuc <mszuc@redhat.com>
2024-06-20 13:49:38 +0200

    fix(UItest): comment out rm kube/config in suite
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/test/ui/PublicTestsSuite.java
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/PublicTestsSuite.java
@@ -19,7 +19,7 @@ public class PublicTestsSuite {
     @BeforeAll
     public static void setUp() {
         KubeConfigUtility.backupKubeConfig();
-        KubeConfigUtility.removeKubeConfig();
+//        KubeConfigUtility.removeKubeConfig();
     }
 
     @AfterAll
