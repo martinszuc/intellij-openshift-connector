b6e65e20593c65ed8e98d594a6bb9a59d0046541
Martin Szuc <mszuc@redhat.com>
2024-07-31 12:56:23 +0200

    feat(UITEST): Start/Stop Dev mode on cluster
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/test/ui/tests_cluster/CreateComponentTest.java
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/tests_cluster/CreateComponentTest.java
@@ -1,8 +1,11 @@
 package org.jboss.tools.intellij.openshift.test.ui.tests_cluster;
 
+import com.intellij.remoterobot.fixtures.ComponentFixture;
 import com.intellij.remoterobot.utils.WaitForConditionTimeoutException;
 import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.idestatusbar.IdeStatusBar;
+import org.jboss.tools.intellij.openshift.test.ui.common.TerminalPanelFixture;
 import org.jboss.tools.intellij.openshift.test.ui.dialogs.component.CreateComponentDialog;
+import org.jboss.tools.intellij.openshift.test.ui.views.OpenshiftView;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,8 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
+import static com.intellij.remoterobot.search.locators.Locators.byXpath;
+import static org.jboss.tools.intellij.openshift.test.ui.utils.constants.XPathConstants.JB_TERMINAL_PANEL;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
@@ -43,10 +48,46 @@ public class CreateComponentTest extends AbstractClusterTest {
         createComponentDialog.setName(COMPONENT_NAME);
         createComponentDialog.selectComponentType("Go Runtime", robot);
         createComponentDialog.setStartDevMode(true);
+        createComponentDialog.selectProjectStarter("go-starter");
         createComponentDialog.clickCreate();
 
         robot.find(IdeStatusBar.class).waitUntilAllBgTasksFinish();
 
+
         ProjectClusterTest.verifyProjectHasItem("newtestproject", COMPONENT_NAME);
     }
+
+    @Test
+    @Order(3)
+    public void startStopDevModeOnClusterComponentTest() {
+        String COMPONENT_NAME = "test-component";
+        OpenshiftView openshiftView = robot.find(OpenshiftView.class, Duration.ofSeconds(2));
+        openshiftView.openView();
+        openshiftView.expandOpenshiftExceptDevfile();
+        openshiftView.menuRightClickAndSelect(robot, 2, "Start dev on Cluster");
+
+
+        robot.find(IdeStatusBar.class).waitUntilAllBgTasksFinish();
+        assertDevModeStarted();
+    }
+
+
+    private void assertDevModeStarted() {
+        // Use TerminalPanelFixture to interact with the terminal panel
+        TerminalPanelFixture terminalPanel = robot.find(TerminalPanelFixture.class, Duration.ofSeconds(60));
+
+        // Get the terminal text using the custom fixture method
+        String terminalText = terminalPanel.getTerminalText();
+        // Log the terminal output for inspection
+        LOGGER.info("Terminal output: {}", terminalText);
+
+        // Ensure the terminal text is not null
+        assertNotNull(terminalText, "Terminal output should not be null");
+
+
+
+        // Further parse or check the terminal output for specific success indicators
+        // For example, you might look for specific log entries or success messages
+    }
+
 }
